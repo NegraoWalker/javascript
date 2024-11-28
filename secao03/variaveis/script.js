bd77672b939e7bd334450ddf289ad0dae7fea54d
Walker Negrão <c0662240@vale.com>
2024-11-28 16:28:37 -0300

    Atualização secao03

---
+++ secao03/variaveis/script.js
@@ -0,0 +1,14 @@
+let primeiraMensagem = "Oi meu nome é Walker";
+const segundaMensagem = "Tenho 31 anos";
+
+console.log(primeiraMensagem);
+console.log(segundaMensagem);
+
+primeiraMensagem = "Oi meu nome é João";
+console.log(primeiraMensagem);
+
+var terceiraMensagem = "Sou de Londrina - PR"; //Forma legada
+console.log(terceiraMensagem);
+
+terceiraMensagem = "Sou de Maringá - PR";
+console.log(terceiraMensagem);
