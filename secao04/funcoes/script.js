92dbaf9861a81aca2f2dce8d98bc4148fdc499fb
Walker Negrão <c0662240@vale.com>
2024-12-05 14:04:10 -0300

    Implementado funcoes como expressões

---
+++ secao04/funcoes/script.js
@@ -0,0 +1,10 @@
+// function nomeDaFuncao(parametros) { //Sintaxe padrão de declaração de funções
+//instruções...
+// }
+
+function saudacao(nome) {
+  console.log(`Olá ${nome}, como está seu dia?`);
+}
+
+saudacao("Walker");
+saudacao("Isabela");
---
+++ secao04/funcoesComoExpressoes/script.js
@@ -0,0 +1,6 @@
+let total = function (valor1, valor2) {
+  return valor1 + valor2;
+};
+
+console.log(total(1, 4));
+console.log(total(5, 6));
