f7599c44d6f77a48084c82b35a6002729d2a17fd
walkerNegrão <walker.esteves.negrao@gmail.com>
2024-12-23 16:07:38 -0300

    Atualização - Objetos Globais

---
+++ secao05/objetosGlobais/date.js
@@ -0,0 +1,15 @@
+/*
+Object
+Number
+String
+Array
+Boolean
+...
+*/
+
+//O objeto Date deve ser instanciado para ser usado:
+const dataAtual = new Date();
+console.log(dataAtual);
+
+console.log(dataAtual.getFullYear());
+console.log(dataAtual.getMonth());
