c03123a65da756f726fbb762e7238e809b72d069
Walker Negrao <1882452@tcs.com>
2025-04-25 09:08:21 -0300

    Atualização

---
+++ conteudos-de-estudo/exercicios/metodos-nativos-objetos/9.js
@@ -0,0 +1,45 @@
+const venda = {
+    produto: "TV",
+    quantidade: 1,
+    precoUnitario: 1999.89,
+    desconto: 5,
+
+    calcularTotal() {
+        return this.quantidade * this.precoUnitario - (this.desconto / 100) * this.precoUnitario;
+    },
+
+    detalharVenda() {
+    return `Detalhe da venda: 
+Nome do produto: ${this.produto} 
+Quantidade: ${this.quantidade} 
+Preço do produto: ${this.precoUnitario}
+Desconto: ${this.desconto}
+Valor a pagar: R$${this.calcularTotal().toFixed(2)}
+`
+    }
+}
+
+venda.aplicarDesconto = function (desconto) {
+    this.desconto = desconto;
+}
+
+venda.atualizarQuantidade = function (quantidade) {
+    this.quantidade = quantidade;
+}
+
+venda.verificarEstoque = function (quantidadeDisponivel) {
+    if(quantidadeDisponivel >= this.quantidade) {
+        return "Venda pode ser realizada";
+    }
+    return "Venda não pode ser realizada";
+}
+
+venda.aplicarDesconto(10);
+venda.atualizarQuantidade(2);
+
+const novaVenda = Object.assign({}, venda, {
+    produto: "Notebook",
+    quantidade: 1,
+  });
+  
+console.log(novaVenda);
